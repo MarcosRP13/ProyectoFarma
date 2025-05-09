1e1f9965c34584529f32e0533901579a5e8d4595
Marcos <marope01@alumnatflorida.es>
2025-05-09 13:19:16 +0200

    Procesar Clientes

---
+++ procesarClientes.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Farmaya</title>
+  <link rel="icon" href="logo.png" type="image/png" />
+  <style>
+        * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Segoe UI', sans-serif;
+    }
+
+    nav.navbar {
+      color: white;
+      background-color: rgb(26, 196, 74);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 15px 30px;
+    }
+
+    .logo h1 {
+      font-size: 35px;
+      font-weight: bold;
+    }
+
+    .nav-links {
+      list-style: none;
+      display: flex;
+      gap: 20px;
+    }
+
+    .nav-links a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 12px;
+      border-radius: 6px;
+    }
+
+    .nav-links a:hover {
+      background-color: darkgreen;
+      color: white;
+    }
+
+    footer {
+    background-color: rgb(26, 196, 74);
+    padding: 10px;
+    text-align: center;
+    color: white;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    }
+    
+    footer a {
+      color: white;
+      text-decoration: none;
+    }
+    footer a:hover {
+      color: #ddd;
+    }
+    
+    body {
+      padding-bottom: 100px;
+    }
+</style>
+</head>
+<body>
+
+  <nav class="navbar">
+    <div class="logo"><h1>Farmaya</h1></div>
+    <ul class="nav-links">
+      <li><a href="acerca.php">Acerca</a></li>
+      <li><a href="clientes.php">Clientes</a></li>
+      <li><a href="farmacias.php">Farmacias</a></li>
+      <li><a href="productos.php">Productos</a></li>
+      <li><a href="pedidos.php">Pedidos</a></li>
+      <li><a href="registros.php">Registros</a></li>
+    </ul>
+  </nav>
+  <footer>
+<p>&copy; 2025 Farmaya. Todos los derechos reservados.</p>
+    <p>
+      <a href="principal.php">Inicio</a> |
+      <a href="acerca.php">Acerca de</a> |
+      <a href="registros.php">Contacto</a>
+    </p>
+</footer>
+</body>
+</html>
+<?php
+require_once 'autoloader.php';
+$modelo = new Model();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $codlie = $_POST["codlie"];
+  $nombre = $_POST["nombre"];
+    $ape1 = $_POST["ape1"];
+    $ape2 = $_POST["ape2"];
+    $tel = $_POST["tel"];
+    $email = $_POST["email"];
+    $dir = $_POST["dir"];
+
+    $modelo->insertarCliente($codlie, $nombre, $ape1, $ape2, $tel, $email, $dir);
+}
+
+echo "Usuario añadido: " . $nombre. " " . $ape1;
+
+?>
